a181c26e6fc5a0ed5fff1fbc2d993abb124904c9
Alexander <sharrikk@gmail.com>
2016-04-27 19:33:18 +0300

    init

--- TestHTMLParsing/ViewController.m
+++ TestHTMLParsing/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Ono.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self getItemsWithRequest:@"ONO" completion:^(NSArray *arr, NSError *err) {
+        
+    }];
+}
+
+- (void)getItemsWithRequest:(NSString *)requestStr completion:(void(^)(NSArray *arr, NSError *err))completion {
+    NSString *urlStr = @"https://habrahabr.ru/";
+    NSURL *url = [NSURL URLWithString:urlStr];
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            completion(nil, error);
+        } else {
+            NSArray *arr = [self parseData:data];
+            completion(arr, nil);
+        }
+    }] resume];
+}
+
+- (NSArray *)parseData:(NSData *)data {
+    ONOXMLDocument *doc = [ONOXMLDocument HTMLDocumentWithData:data error:nil];
+    ONOXMLElement *root = [doc rootElement];
+//    NSLog(@"%@", root);
+    [root enumerateElementsWithXPath:@"//div[@class='posts shortcuts_items']/div" usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+        NSDictionary *dictionary = [self parseElement:element];
+        NSLog(@"%@", dictionary);
+    }];
+//    NSLog(@"%@", root);
+    return nil;
+}
+
+- (NSDictionary *)parseElement:(ONOXMLElement *)elem {
+    NSString *title = [[[elem firstChildWithTag:@"h1"] firstChildWithTag:@"a"] stringValue];
+    NSString *imgUrlStr = nil;
+    for (ONOXMLElement *child in [elem childrenWithTag:@"div"]) {
+        if (![child[@"class"] isEqual:@"content html_format"])
+            continue;
+        ONOXMLElement *imgChild = [child firstChildWithTag:@"img"];
+        imgUrlStr = imgChild[@"src"];
+    }
+    NSMutableDictionary *dictionary = [@{@"title" : title} mutableCopy];
+    if (imgUrlStr) {
+        dictionary[@"img"] = imgUrlStr;
+    }
+    return dictionary;
 }
 
 - (void)didReceiveMemoryWarning {
