f66c983712c1b50987c96a81b8f5b83d3cc60639
Alexander <sharrikk@gmail.com>
2016-04-27 18:41:32 +0300

    Initial Commit

---
+++ TestHTMLParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestHTMLParsing
+//
+//  Created by Alexander on 27.04.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestHTMLParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestHTMLParsing
+//
+//  Created by Alexander on 27.04.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
